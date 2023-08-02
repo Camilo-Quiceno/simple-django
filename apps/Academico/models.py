b9f3041a885dc9dd602f92720c8ccf0e51eed4eb
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-02 17:30:40 -0500

    Hello world Django, first template is rendered

--- apps/Academico/models.py
+++ apps/Academico/models.py
@@ -5,3 +5,6 @@ class Course(models.Model):
     code = models.CharField(primary_key=True, max_length=6)
     name = models.CharField(max_length=50)
     credits = models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return f'{self.name} ({self.credits})'
