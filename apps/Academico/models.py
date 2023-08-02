6e7a58f5aa3a5ab2ebd9e0df9035c1f7334a8c94
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-02 07:23:03 -0500

    Migrations and Super User created

--- apps/Academico/models.py
+++ apps/Academico/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    code = models.CharField(primary_key=True, max_length=6)
+    name = models.CharField(max_length=50)
+    credits = models.PositiveSmallIntegerField()
