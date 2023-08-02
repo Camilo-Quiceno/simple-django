8f6dd787f1b5f865c67e87d04162837e449347d7
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-02 17:44:56 -0500

    Using the model in the initial template

--- apps/Academico/views.py
+++ apps/Academico/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 def home(request):
-    return render(request, "manageCourses.html")
+    courses_list = Course.objects.all()
+    return render(request, "manageCourses.html", {"courses": courses_list})
