2501665c5d244e6cd2f0d42627e3daa8df0ff25c
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-02 19:05:37 -0500

    Post add course is implemeted and Edit & Delete buttons created (not implemented)

--- apps/Academico/views.py
+++ apps/Academico/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import RequestContext
 from .models import Course
 
 # Create your views here.
 def home(request):
     courses_list = Course.objects.all()
     return render(request, "manageCourses.html", {"courses": courses_list})
+
+def registerCourse(request):
+    code = request.POST['txtCode']
+    name = request.POST['txtName']
+    credits = request.POST['numCredits']
+    
+    new_course = Course.objects.create(code=code, name=name, credits=credits)
+    return redirect('/', RequestContext(request))
+    
