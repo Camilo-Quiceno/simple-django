f79786c04a967407d55692ced0db68b9670e13a6
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-04 20:39:12 -0500

    Adding messages when the user interact with the form

--- apps/Academico/views.py
+++ apps/Academico/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.template import RequestContext
 from .models import Course
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     courses_list = Course.objects.all()
+    messages.success(request, "Courses Listed!")
     return render(request, "manageCourses.html", {"courses": courses_list})
 
 def registerCourse(request):
@@ -13,6 +15,7 @@ def registerCourse(request):
     credits = request.POST['numCredits']
     
     Course.objects.create(code=code, name=name, credits=credits)
+    messages.success(request, "Courses Registered")
     return redirect('/', RequestContext(request))
 
 def editCourse(request, code):
@@ -28,9 +31,11 @@ def courseEdited(request):
     course.name = name
     course.credits = credits
     course.save()
+    messages.success(request, "Course Edited")
     return redirect('/', RequestContext(request))
 
 def deleteCourse(request, code):
     course = Course.objects.get(code=code)
     course.delete()
+    messages.success(request, "Courses Deleted")
     return redirect('/', RequestContext(request))
